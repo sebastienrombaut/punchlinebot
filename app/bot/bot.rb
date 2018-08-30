e4f2478182c3a549dbc243b74e3a214044a27428
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-08-30 11:26:11 +0200

    Correct variable

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook-messenger'
 
 include Facebook::Messenger
-Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['access_token'])
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
