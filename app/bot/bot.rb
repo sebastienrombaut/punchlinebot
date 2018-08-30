1a8fc54aae38ea1695acb344a996a75deb0d15d3
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-08-30 13:38:28 +0200

    Add messages

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,5 +4,17 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  message.reply(text: 'PAS LOUÉ')
+  message.reply(text: 'T\'es là mamene')
+
+  if message.include? 'photo' || 'image'
+    message.reply(
+      attachment: {
+        type: 'image',
+        payload: {
+          url: 'https://cdn-s-www.lalsace.fr/images/23ca079d-9727-4c69-96e8-0b5d49c75544/BES_06/illustration-lorenzo-salad-tomat-onion_1-1522766737.jpg'
+        }
+      }
+    )
+  end
 end
