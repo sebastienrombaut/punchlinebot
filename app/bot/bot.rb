1c1018fcabf1af21fe0beb016140eb1e6750d330
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-10-10 18:24:21 +0200

    try persona

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,6 +3,17 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
+Bot.deliver({
+  recipient: {
+    id: '45123'
+  },
+  message: {
+    text: 'Human?'
+  },
+  "persona_id" => "<318552875394434>"
+}, access_token: ENV['ACCESS_TOKEN'])
+
+
 Bot.on :message do |message|
   HandleIncomingMessage.new(message).perform
 end
